be9541d42021ff0b8c7ff926ebae14a58f8f1c4f
Albert wang <awang21@ssis.edu.vn>
2019-08-29 16:47:05 +0700

    Update greedy.py

--- greedy.py
+++ greedy.py
@@ -13,16 +13,14 @@ def calculate(change=None):
 #A loop that reprompts user if the number entered is less than 0
     while change < 0:
         change = round(float(input("How much change is owed? ")), 2)
-        if change >= 0:
-            break
+
+        
 #changes the float into a integer to avoid error
     changes = int(change * 100)
     coins = 0
 
 
-#Floor divide the integer change by the coin(25, 10, 5, 1)and add the number
-#to the variable(coin). Then divide the number by coin(25, 10, 5, 1) and set
-#the change = to the remainder.
+#Calculate coins(0.25, 0.10, 0.05, 0.01)
 
     coins += changes // 25
     changes = changes % 25
@@ -41,7 +39,7 @@ def calculate(change=None):
 #will print out you need 1 coin. If it is larger or equal to 0, it will print
 #out coins.
 
-    
+
     if coins > 1 or coins == 0:
         print("You need {0} coins".format(coins))
     else:
